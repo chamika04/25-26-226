733b175d24a5223abeeb534287f7464281ac7972
Lochana Jayasinghe <lochanajayasinghe2002@gmail.com>
2025-12-14 15:22:51 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Landing />
     </div>
   );
 }
