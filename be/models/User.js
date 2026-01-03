db102f31fb2492480fd0d82fb7897d50d6d38406
samadhi wijethunga <raswijethunga202@gmail.com>
2026-01-03 13:55:15 +0530

    be_1

---
+++ be/models/User.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: [
+        "pharmacist",
+        "chief_pharmacist",
+        "store_manager",
+        "ward_nurse",
+        "admin"
+      ],
+      required: true
+    },
+    firstName: {
+      type: String
+    },
+    lastName: {
+      type: String
+    },
+    mobile: {
+      type: Number
+    },
+    address: {
+      type: String
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
+
+
+/* 
+1 - admin
+2 - pharmacist, chief pharmacist, ward nurse. (to check regarding the medicine and)
+3 - doctor (for the doctor availability component)
+4 - head nurse of all the wards, ward head nurse (to check bed occupency)
+5 - few roles for seasonal illness part
+6 - inventory manager (to manage medicine, equipemnts) 
+
+*/
